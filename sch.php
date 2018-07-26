eec3723658ce1ca0bd7f0a4f45f012f22b69b37e
papalex <35659343+papalex@users.noreply.github.com>
2018-07-26 12:43:56 +0300

    Create sch.php

---
+++ sch.php
@@ -0,0 +1,36 @@
+<?php
+?>
+<svg width="100" height="100">
+		<defs>		
+			<g id="voltdcico">
+				<rect width="50" height="50" rx="3" ry="3" style="fill:none; stroke:black;"></rect>
+				<path stroke="#555" stroke-width="2" d="M6 44v-25a3,3 0 1 0 1 0 M43 44v-25a3,3 0 1 0 1 0"></path>		
+				<path d="M18 20H41m0 -12v1m-35 0h1m-1 0v1" style="stroke-dasharray: 1, 9;stroke:#444; stroke-width: 10;"></path>
+				<path d="M13 20H41" style="stroke-dasharray: 1, 9;stroke:#444; stroke-width: 20;"></path>
+			</g>
+			<g id="swico">	
+				<rect width="50" height="50" rx="3" ry="3" style="fill:none; stroke:black;"></rect>		
+				<path fill="#eee" d="M10 18h25l9 18h-24l-10 -18"></path>
+				<path fill="#aaa" d="M10 18l10 18v6l-10 -16v-9"></path>
+				<path fill="#ccc" d="M21 36h24v6h-24v-6"></path>
+				<path fill="#444" d="M20 21h9l7 12h-9l-8 -12"></path>
+				<path d="M23 12 v13a5,5 0 0 0 6 0v-13h-6" style="fill:white;stroke:black;"></path>
+				<path d="M23 17 v-1 a6,6 0 1 1 6 0 v1h-6" style="fill:#b01010;stroke:black;"></path>
+				<path d="M27 8 l2 -1 a5,5 0 0 1 1 5 A5,5 0 0 0 27 8" style="fill:white;"></path>
+			</g>
+			<path id="reloadico" d="M 50 10 A 40 40 0 1 0 50 90M 74 43 A 25 25 0 1 1 50 25l -2 -5 l 17 10 l -15 4 l 1 -4 A 20 20 0 1 0 69 45 l 5 -2 M 50 90 A 40 40 0 1 0 50 10" fill="#3eb064" stroke="#00aaff" stroke-width="1"></path>
+		</defs>
+		<g >
+		<use xlink:href="#reloadico"></use>
+		</g>
+  <g >
+		
+		</g>
+	</svg>
+      <svg><g >
+		<use transform="translate(100) rotate(90)" xlink:href="#voltdcico"></use>
+        <g transform="translate(150,7)" id="resist_r1"><text x="15" y="30" >R1</text>
+        <rect  width="50" height="15" rx="1" ry="1" style="fill:none; stroke:black;"></rect></g>
+          <path d="m100 15 h50" fill="#3eb064" stroke="#000" stroke-width="2"></path>
+          <path d="m200 15 h25" fill="#3eb064" stroke="#000" stroke-width="2"></path>
+		</g></svg>
